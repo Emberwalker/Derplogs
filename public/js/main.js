129fd3723f8a2b0cbf501e75e7b21b00d19180f5
Hamish Hutchings <hamhut1066@gmail.com>
2014-12-08 17:17:52 +0000

    fix logger and add functions for all occasions

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,15 @@ var Ws = function(uri) {
     var onOpen = function() {
         console.log('connection opened');
     }
+    var onClose = function() {
+        console.log('connection closed');
+    }
+    var onMessage = function() {
+        console.log('connection message');
+    }
+    var onError = function() {
+        console.log('connection error');
+    }
 
     var ws = new WebSocket(uri);
     ws.onopen = function(evt ) { onOpen(evt); }
@@ -13,9 +22,13 @@ var Ws = function(uri) {
     ws.onerror = function(evt ) { onError(evt); }
 
 
-    return this;
+    return {
+        send: function(msg) {
+            ws.send(msg);
+        }
+    }
 }
 
-var wsUri = 'ws://188.226.165.41:9000';
+var wsUri = 'ws://188.226.165.41:9000/socket';
 var w = Ws(wsUri);
 
