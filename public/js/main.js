9923cb851c86c3b9033343f33d8e3cea39f46f34
Hamish Hutchings <hamhut1066@gmail.com>
2014-12-08 17:39:53 +0000

    add code for the display

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,40 @@
 "use strict"
 
+var Display = function() {
+    // used for display logic
+
+    var list = document.getElementById('list');
+
+    var process = function(msg, repo, committer) {
+        //do more stuff here
+        log(msg, 'INFO');
+    }
+
+    return {
+        newLog: function(msg, repo, committer) {
+            process(msg, repo, committer);
+        }
+    }
+}
 var Ws = function(uri) {
 
-    var onOpen = function() {
-        console.log('connection opened');
+    var onOpen = function(evt) {
+        log('connection opened', 'INFO');
     }
-    var onClose = function() {
-        console.log('connection closed');
+    var onClose = function(evt) {
+        log('connection closed', 'INFO');
     }
-    var onMessage = function() {
-        console.log('connection message');
+    var onMessage = function(evt) {
+        log('message recieved', 'INFO');
+        var data = JSON.parse(evt.data);
+        log(JSON.stringify(data), 'DEBUG');
+        d.newLog(data.msg, data.repo, data.committer);
     }
-    var onError = function() {
-        console.log('connection error');
+    var onError = function(evt) {
+        log('connection error', 'ERROR');
     }
 
+    var d = Display();
     var ws = new WebSocket(uri);
     ws.onopen = function(evt ) { onOpen(evt); }
     ws.onclose = function(evt ) { onClose(evt); }
